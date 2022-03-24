01dc37d090f74d3ae613fa764f6ea585317fb714
Jason-Ying <jasonying2021@gmail.com>
2022-03-24 23:10:33 +0800

    Update query.js

--- assets/scripts/query.js
+++ assets/scripts/query.js
@@ -10,6 +10,29 @@ function getResidentList(unit, checked, ability) {
 			for (let i = 0; i < result.split(";").length - 1; i++) {
 				resi[i] = result.split(";")[i].split(",");
 			} // unit room name id phone checked live work move
+			function changearr(oldarr) {
+				var maxLength = getMaxLength(oldarr);
+				var rowarr = [], rowoldarr = [];
+				for (var k = 0; k < oldarr.length; k++) {
+					for (var m = 0; m < maxLength; m++) {
+						if (!rowarr[m]) rowarr[m] = [];
+						rowarr[m].push(oldarr[k][m]);
+						if (!rowoldarr[m]) rowoldarr[m] = [];
+						rowoldarr[m].push(oldarr[k][m]);
+					}
+				}
+				return rowarr;
+			}
+			function getMaxLength(arr) {
+				var max = 0;
+				for (var i = 0; i < arr.length; i++) {
+					if (arr[i].length > max) {
+						max = arr[i].length;
+					}
+				}
+				return max;
+			}
+			resi = changearr(resi);
 			[buildingInfo, roomInfo, nameInfo, genderInfo, idInfo, phoneInfo, loginInfo, liveInfo, workInfo, moveInfo] = resi;
 		},
 		error: function () {
