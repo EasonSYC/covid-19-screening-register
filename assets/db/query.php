d2a6773a8d7be77e9e671ed90557d4acd0e2f7fd
Jason-Ying <jasonying2021@gmail.com>
2022-03-24 17:50:52 +0800

    add query

---
+++ assets/db/query.php
@@ -0,0 +1,42 @@
+<?php
+@header("content-type:text/html;charset=utf8");
+$conn = mysqli_connect("localhost", "root", "123456") or die("FATAL_ERR" . mysqli_error($conn));
+$select = mysqli_select_db($conn, 'pim');
+$utf = mysqli_query($conn, "set names utf8");
+
+
+$unit = $_POST["unit"];
+$checked = $_POST["checked"];
+$ability = $_POST["ability"];
+
+
+$query = "";
+if ($unit != "-1") $query += "unit = " + $unit + " and ";
+if ($checked != "-1") $query += "checked = " + $checked + " and ";
+if ($ability != "-1") $query += "ability = " + $ability + " and ";
+if ($query != "") {
+	$query = substr($query, 0, -5);
+	$query = "where " + $query;
+}
+
+$sql = mysqli_query($conn, "select * from resi " + $query);
+
+$i = 0;
+$row = mysqli_fetch_array($sql);
+do {
+	$rows[$i] = $row;
+	$i++;
+} while ($row = mysqli_fetch_array($sql));
+$rowall = '';
+for ($i = 0; $i < count($rows); $i++) {
+	for ($j = 0; $j < 9; $j++) {
+		$rowall = $rowall . $rows[$i][$j] . ',';
+	}
+	$rowall = $rowall . $rows[$i][$j] . ';';
+}
+
+if (1 == 0) {
+	echo json_encode('err');
+} else {
+	echo json_encode($rowall);
+}
---
+++ assets/scripts/query.js
@@ -0,0 +1,21 @@
+function getResidentList(unit, checked, ability) {
+	let datas = { unit: unit, checked: checked, ability: ability };
+	let buildingInfo = new Array(), roomInfo = new Array(), nameInfo = new Array(), genderInfo = new Array(), idInfo = new Array(),
+	phoneInfo = new Array(), loginInfo = new Array(), liveInfo = new Array(), workInfo = new Array(), moveInfo = new Array();
+	$.ajax({
+		url: './assets/db/query.php',
+		type: 'post',
+		dataType: 'json',
+		data: datas,
+		async: false,
+		success: function (result) {
+			for (let i = 0; i < result.split(";").length - 1; i++) {
+				resi[i] = result.split(";")[i].split(",");
+			} // unit room name id phone checked live work move
+			[buildingInfo, roomInfo, nameInfo, genderInfo, idInfo, phoneInfo, loginInfo, liveInfo, workInfo, moveInfo] = resi;
+		},
+		error: function () {
+			alert("FATAL_ERR: ERR_QUERY_PHP");
+		}
+	})
+}
